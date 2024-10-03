680cb26e7840ab441175ffc2a18899af84f3431c
zcoakley <157329265+zcoakley@users.noreply.github.com>
2024-10-02 20:49:08 -0400

    Update sensor_test.ino

--- sensor_test.ino
+++ sensor_test.ino
@@ -1,7 +1,7 @@
 // Code for sensors that detects if it sees the tape or not
 
 // Pin setup
-const int sensorPin = 2;  // Phototransistor output connected to digital pin 2
+const int sensorPin = A0;  // Phototransistor output
 
 void setup() {
   pinMode(sensorPin, INPUT);   // Set sensor pin as input
@@ -10,15 +10,15 @@ void setup() {
 
 void loop() {
   // Read the sensor value
-  int sensorValue = digitalRead(sensorPin);
+  int sensorValue = analogRead(sensorPin);
+
+  Serial.println(sensorValue);
   
-  if (sensorValue == HIGH) {
-    digitalWrite(ledPin, HIGH);
-    Serial.println("floor");
-  } else {
-    digitalWrite(ledPin, LOW);
-    Serial.println("tape");
-  }
+  // if (sensorValue == HIGH) {
+  //   Serial.println("HIGH");
+  // } else {
+  //   Serial.println("LOW");
+  // }
 
   delay(100);  // Wait for 100 milliseconds before the next read
 }
